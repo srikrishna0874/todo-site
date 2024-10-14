63be5518416c89154754e2e0966b0d0bd72384cf
Kollipara Sri Krishna Venkata Subbarao <103669180+srikrishna0874@users.noreply.github.com>
2024-10-14 15:13:36 +0530

    frontend completed

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const path=require("path");
 const app = express();
 
+app.use("/public", express.static("public"));
 
-app.get('/',(req,res)=> {
-    res.send('hello world');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
-app.listen(3000, () =>{
+app.listen(3000, () => {
     console.log('server is running');
 });
