a92f3c41ad10b24255d8b783433b494c4dbf5fe5
Kollipara Sri Krishna Venkata Subbarao <103669180+srikrishna0874@users.noreply.github.com>
2024-10-16 19:49:31 +0530

    started backend

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require("express");
-const path=require("path");
 const app = express();
+const path=require('path');
+
+
 
 app.use("/public", express.static("public"));
 
-
-app.use(require("./routes/index.js"));
 
 
 app.listen(3000, () => {
     console.log('server is running');
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+});
+
+app.post('/add_task', (req, res) => {
+    console.log('body is: ');
+    console.log(req.body);
+});
+
+
+
+
